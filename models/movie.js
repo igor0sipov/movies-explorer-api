84c64dfde1b43b8473c90721ff543253d44c007b
Igor Osipov <fakealien@yandex.ru>
2021-03-09 23:34:14 +0300

    feat: movie mongoose schema

---
+++ models/movie.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    validate: {
+      validator(url) {
+        const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
+
+        return regex.test(url);
+      },
+      message: 'Некорректная ссылка',
+    },
+    required: true,
+  },
+  trailer: {
+    type: String,
+    validate: {
+      validator(url) {
+        const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
+
+        return regex.test(url);
+      },
+      message: 'Некорректная ссылка',
+    },
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    validate: {
+      validator(url) {
+        const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
+
+        return regex.test(url);
+      },
+      message: 'Некорректная ссылка',
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
