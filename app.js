4f8d498eb3615202b90a627d196d63b01607d830
Igor Osipov <fakealien@yandex.ru>
2021-03-09 22:49:08 +0300

    feat: mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/favfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
