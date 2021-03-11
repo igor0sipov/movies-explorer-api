e8fa7625d371a573e0e8a1f5308f93bd1f47faae
Igor Osipov <fakealien@yandex.ru>
2021-03-11 23:10:09 +0300

    feat: movie routes validation

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,29 @@
+const { Joi, celebrate } = require('celebrate');
+
 const moviesRouter = require('express').Router();
 const { getMovies, addMovie, deleteMovie } = require('../controllers/movies');
 
 moviesRouter.get('/movies', getMovies);
 
-moviesRouter.post('/movies', addMovie);
+moviesRouter.post('/movies', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required().min(2).max(60),
+    director: Joi.string().required().min(2).max(60),
+    duration: Joi.number().required().min(2).max(30),
+    year: Joi.string().required().length(4),
+    description: Joi.string().required().min(2),
+    image: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required(),
+    trailer: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required(),
+    nameRU: Joi.string().required().min(2).max(30),
+    nameEN: Joi.string().required().min(2).max(30),
+    thumbnail: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required(),
+  }),
+}), addMovie);
 
-moviesRouter.delete('/movies/movieId', deleteMovie);
+moviesRouter.delete('/movies/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required().length(24).hex(),
+  }),
+}), deleteMovie);
 
 module.exports = moviesRouter;
