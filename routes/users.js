c2842c2ba9a0d527cc534fb96ff712298e5b2a41
Igor Osipov <fakealien@yandex.ru>
2021-03-11 23:29:07 +0300

    feat: user routes validation

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const { Joi, celebrate } = require('celebrate');
+
 const usersRouter = require('express').Router();
 const {
   getCurrentUser,
@@ -6,6 +8,11 @@ const {
 
 usersRouter.get('/users/me', getCurrentUser);
 
-usersRouter.patch('users/me', editUserInfo);
+usersRouter.patch('users/me', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), editUserInfo);
 
 module.exports = usersRouter;
