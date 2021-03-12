c4407837b1d6e9305810fca9049e92555ab208d0
Igor Osipov <fakealien@yandex.ru>
2021-03-12 21:31:44 +0300

    feat: login controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 const NotFoundError = require('../errors/not-found-error');
 const BadRequestError = require('../errors/bad-request-error');
 const ConflictError = require('../errors/conflict-error');
+const UnauthorizedError = require('../errors/unauthorized-error');
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user._id).then((currentUser) => {
@@ -61,6 +63,35 @@ module.exports.createUser = (req, res, next) => {
           throw new BadRequestError(err.message);
         }
       });
+    })
+    .catch(next);
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  const { NODE_ENV, JWT_SECRET } = process.env;
+  const STUDENT_JWT_SECRET = '908f8f3afc1d8d6933041c9134ca019993aaee7ab6685648abce113cd738731f';
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production'
+          ? JWT_SECRET
+          : STUDENT_JWT_SECRET,
+        { expiresIn: '7d' },
+      );
+
+      res
+        .cookie('token', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .send({ message: 'Успешно!' })
+        .end();
+    })
+    .catch((err) => {
+      throw new UnauthorizedError(err.message);
     })
     .catch(next);
 };
