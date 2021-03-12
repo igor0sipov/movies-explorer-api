fcd684183ca054d2c84b0e929e555d211ac8fed2
Igor Osipov <fakealien@yandex.ru>
2021-03-12 21:38:22 +0300

    feat: auth added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
+const { Joi, celebrate, errors } = require('celebrate');
 const users = require('./routes/users');
 const movies = require('./routes/movies');
 const NotFoundError = require('./errors/not-found-error');
+const { createUser, login, signout } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,8 +18,25 @@ mongoose.connect('mongodb://localhost:27017/favfilmsdb', {
 
 app.use(express.json());
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(60),
+    password: Joi.string().required().min(8).max(32),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(60),
+    password: Joi.string().required().min(8).max(32),
+    name: Joi.string().min(2).max(32),
+  }),
+}), createUser);
+
 app.use('/', users);
 app.use('/', movies);
+
+app.post('/signout', signout);
 
 app.use('*', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
