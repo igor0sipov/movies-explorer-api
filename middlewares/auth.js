35c6e930d2eb35fd8a16258b62f56bc4a8bafa4f
Igor Osipov <fakealien@yandex.ru>
2021-03-13 20:29:12 +0300

    feat: error texts from config

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
+const { errorText } = require('../configs/config');
 
 module.exports = (req, res, next) => {
   const { token } = req.cookies;
   const { JWT_SECRET } = process.env;
   if (!token) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError(errorText.authorizationRequired);
   }
 
   let payload;
@@ -13,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (error) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError(errorText.authorizationRequired);
   }
 
   req.user = payload;
