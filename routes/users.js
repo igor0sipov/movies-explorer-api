6bb56382f7aa21e65f3ea37309e4688d72f1b0e3
Igor Osipov <fakealien@yandex.ru>
2021-03-13 20:36:22 +0300

    fix: route path

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const {
 
 usersRouter.get('/users/me', getCurrentUser);
 
-usersRouter.patch('users/me', celebrate({
+usersRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(2).max(30),
     name: Joi.string().required().min(2).max(30),
