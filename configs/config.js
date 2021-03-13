8828c304adb2c5bee353c43b6f3f35448a1aec50
Igor Osipov <fakealien@yandex.ru>
2021-03-13 20:28:03 +0300

    feat: config file

--- configs/config.js
+++ configs/config.js
@@ -1,4 +1,8 @@
 module.exports.errorText = {
   resourceNotFound: 'Запрашиваемый ресурс не найден',
-
+  someoneElsesMovie: 'Нельзя удалять чужие фильмы',
+  movieNotFound: 'Нет фильма с таким id',
+  userNotFound: 'Нет пользователя с таким id',
+  sameUser: 'Пользователь с таким email уже существует',
+  authorizationRequired: 'Необходима авторизация',
 };
