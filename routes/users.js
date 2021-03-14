8225a6ca4264804f773f5defd4bc9de4d04a14b2
Igor Osipov <fakealien@yandex.ru>
2021-03-14 22:36:54 +0300

    fix: error texts using config

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,16 @@ const {
   getCurrentUser,
   editUserInfo,
 } = require('../controllers/users');
+const { getErrorText } = require('../configs/config');
 
 usersRouter.get('/users/me', getCurrentUser);
 
 usersRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
-    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().min(2).max(30)
+      .message(getErrorText('email')),
+    name: Joi.string().required().min(2).max(30)
+      .message(getErrorText('имя(name)')),
   }),
 }), editUserInfo);
 
