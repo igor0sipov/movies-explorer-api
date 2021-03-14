fe0517e58fa1e2407e9ae3ab4a88c2045e2fd155
Igor Osipov <fakealien@yandex.ru>
2021-03-14 22:36:48 +0300

    fix: error texts using config

--- routes/movies.js
+++ routes/movies.js
@@ -1,23 +1,35 @@
-const { Joi, celebrate } = require('celebrate');
-
 const moviesRouter = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
+const { getErrorText } = require('../configs/config');
 const { getAllMovies, addMovie, deleteMovie } = require('../controllers/movies');
 
 moviesRouter.get('/movies', getAllMovies);
 
 moviesRouter.post('/movies', celebrate({
   body: Joi.object().keys({
-    country: Joi.string().required().min(2).max(60),
-    director: Joi.string().required().min(2).max(60),
-    duration: Joi.number().required(),
-    year: Joi.string().required().length(4),
-    description: Joi.string().required().min(2),
-    image: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required(),
-    trailer: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required(),
-    nameRU: Joi.string().required().min(2).max(30),
-    nameEN: Joi.string().required().min(2).max(30),
-    thumbnail: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required(),
+    country: Joi.string().required().min(2).max(60)
+      .messages(getErrorText('страна(country)')),
+    director: Joi.string().required().min(2).max(60)
+      .messages(getErrorText('режисер(director)')),
+    duration: Joi.number().required()
+      .messages(getErrorText('длительность(duration)')),
+    year: Joi.string().required().length(4)
+      .messages(getErrorText('год(year)')),
+    description: Joi.string().required().min(2)
+      .messages(getErrorText('описание(description)')),
+    image: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required()
+      .messages(getErrorText('изображение(image)')),
+    trailer: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required()
+      .messages(getErrorText('трейлер(trailer)')),
+    nameRU: Joi.string().required().min(2).max(30)
+      .messages(getErrorText('русское название(nameRU)')),
+    nameEN: Joi.string().required().min(2).max(30)
+      .messages(getErrorText('английское название(nameEN)')),
+    thumbnail: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required()
+      .messages(getErrorText('миниатюра(thumbnail)')),
   }),
+}, {
+
 }), addMovie);
 
 moviesRouter.delete('/movies/:movieId', celebrate({
