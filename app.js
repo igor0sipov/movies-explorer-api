3f09a344292a655f271f32a8269dd68b012332ad
Igor Osipov <fakealien@yandex.ru>
2021-03-14 22:37:35 +0300

    helmet & rate-limiter module added

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const limiter = require('./configs/rateLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const index = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
+const { dbName } = require('./configs/config');
 
 const { PORT = 3000, DB_NAME, NODE_ENV } = process.env;
 
 const app = express();
 
+app.use(limiter);
+
 app.use(cookieParser());
 
-mongoose.connect(`mongodb://localhost:27017/${NODE_ENV === 'production' ? DB_NAME : 'favfilmsdb'}`, {
+mongoose.connect(`mongodb://localhost:27017/${NODE_ENV === 'production' ? DB_NAME : dbName}`, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -22,6 +27,8 @@ mongoose.connect(`mongodb://localhost:27017/${NODE_ENV === 'production' ? DB_NAM
 app.use(express.json());
 
 app.use(requestLogger);
+
+app.use(helmet());
 
 app.use(index);
 
