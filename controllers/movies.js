18dfefc7d818975d3a61784530b47c5aacc4680d
Igor Osipov <fakealien@yandex.ru>
2021-03-17 01:00:12 +0300

    fix: movieId sending added

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,16 +3,14 @@ const User = require('../models/user');
 const BadRequestError = require('../errors/bad-request-error');
 const ForbiddenError = require('../errors/forbidden-error');
 const NotFoundError = require('../errors/not-found-error');
-const { someoneElsesMovie, movieNotFound } = require('../configs/config');
+const { someoneElsesMovie, movieNotFound, sameMovie } = require('../configs/config');
+const CoflictError = require('../errors/conflict-error');
 
 module.exports.getAllMovies = (req, res, next) => {
   Movie.find({}).then((movieList) => res.send(movieList)).catch(next);
 };
 
 module.exports.addMovie = (req, res, next) => {
-  const randomMovieId = [...Array(24)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-  // временное решение
-
   const {
     country,
     director,
@@ -21,12 +19,13 @@ module.exports.addMovie = (req, res, next) => {
     description,
     image,
     trailer,
+    movieId,
     nameRU,
     nameEN,
     thumbnail,
   } = req.body;
 
-  User.findById(req.user._id)
+  Movie.init().then(() => User.findById(req.user._id))
     .then((owner) => Movie.create([{
       owner,
       country,
@@ -38,11 +37,14 @@ module.exports.addMovie = (req, res, next) => {
       trailer,
       nameRU,
       nameEN,
-      movieId: randomMovieId,
+      movieId,
       thumbnail,
     }], { runValidators: true }))
     .then((movie) => res.send(movie[0]))
     .catch((err) => {
+      if (err.message.startsWith('E11000')) {
+        throw new CoflictError(sameMovie);
+      }
       throw new BadRequestError(err);
     })
     .catch(next);
