3f4adc622f131de8279913a855beb673d61c5b85
Igor Osipov <fakealien@yandex.ru>
2021-03-17 01:02:48 +0300

    fix: movieId added

--- routes/movies.js
+++ routes/movies.js
@@ -21,6 +21,8 @@ moviesRouter.post('/movies', celebrate({
       .messages(getErrorText('изображение(image)')),
     trailer: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i).required()
       .messages(getErrorText('трейлер(trailer)')),
+    movieId: Joi.number().required()
+      .messages(getErrorText('movie id')),
     nameRU: Joi.string().required().min(2).max(30)
       .messages(getErrorText('русское название(nameRU)')),
     nameEN: Joi.string().required().min(2).max(30)
