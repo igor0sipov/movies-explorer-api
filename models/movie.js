3956be87855d56fa1b00d38797c0fe8a59495960
Igor Osipov <fakealien@yandex.ru>
2021-03-17 01:02:10 +0300

    fix: movieId type & unique

--- models/movie.js
+++ models/movie.js
@@ -64,8 +64,9 @@ const movieSchema = new mongoose.Schema({
     required: true,
   },
   movieId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Number,
     required: true,
+    unique: true,
   },
   nameRU: {
     type: String,
