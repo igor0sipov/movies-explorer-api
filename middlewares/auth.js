cc18c7ac1521c07f36a631585e600343f088bc70
Igor Osipov <fakealien@yandex.ru>
2021-03-16 22:08:14 +0300

    fix: node_env mode check

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
-const { authorizationRequired } = require('../configs/config');
+const { authorizationRequired, devJwtSecret } = require('../configs/config');
 
 module.exports = (req, res, next) => {
   const { token } = req.cookies;
-  const { JWT_SECRET } = process.env;
+  const { JWT_SECRET, NODE_ENV } = process.env;
   if (!token) {
     throw new UnauthorizedError(authorizationRequired);
   }
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : devJwtSecret);
   } catch (error) {
     throw new UnauthorizedError(authorizationRequired);
   }
